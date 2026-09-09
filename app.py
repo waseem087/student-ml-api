f6415b18f116d64afc9cfa155b2906917ca4635b
Muhammad Waseem Akhtar <152527835+waseem087@users.noreply.github.com>
2026-09-09 22:33:53 +0500

    feat: add model metadata to health endpoint (#3)

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ def health():
     return {
         "status": "healthy",
         "application": "student-ml-api",
-        "version": VERSION
+        "application_version": "1.1.0",
+        "model_version": "model-1"
     }
 
 
